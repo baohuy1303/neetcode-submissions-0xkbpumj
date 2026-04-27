5537158b4029b93fad97cc47b31795f835335a30
Huy B. Huynh <huynhbaohuy130333@gmail.com>
2026-04-27 05:38:01 -0500

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # for loop to create new branch at each index
+        res = []
+
+        def dfs(index, tracker, path):
+            if len(path) == len(nums):
+                res.append(path.copy())
+                return
+
+            if index >= len(nums):
+                return
+            
+            for i in range(0, len(nums)):
+                if tracker[i] == True:
+                    continue
+                tracker[i] = True
+                path.append(nums[i])
+                dfs(i, tracker, path)
+                tracker[i] = False
+                path.pop()
+            return
+
+        dfs(0, [False] * len(nums), [])
+        return res
