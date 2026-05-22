939ccd78ce99710b3bf3e7d26a1aa1ba5fad2e4e
Huy B. Huynh <huynhbaohuy130333@gmail.com>
2026-05-22 02:04:05 -0500

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # go backwards
+        # keep track of index that jumps to end
+        # ask can reach end? then can reach cur?
+        # memoization should work O(n) but recursion stack max 1000 so idk
+        
+        n = len(nums) - 1
+        cur = n
+        print(cur)
+        for i in range(len(nums) - 1, -1, -1):
+            print(cur)
+            if nums[i] + i >= n:
+                cur = i
+            else:
+                if nums[i] + i >= cur:
+                    cur = i
+        
+        if cur <= 0:
+            return True
+        return False
+            
