3e2cf25ce396cc3b93c282dfa29ece7736a676e7
Huy B. Huynh <huynhbaohuy130333@gmail.com>
2026-05-25 23:17:00 -0500

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # have to use/split all of s
+        # use a word, the rest is scanned
+        # can reuse as many words in word dict
+        # if remaining cant be found in dict, return False
+        memo = {}
+        def dfs(i):
+            if i >= len(s):
+                return True
+            if i in memo:
+                return memo[i]
+            for j in range(len(s) - i):
+                end = i + j
+                if s[i:end + 1] in wordDict:
+                    if dfs(end+1) == True:
+                        return True
+            memo[i] = False
+            return memo[i]
+        return dfs(0)
