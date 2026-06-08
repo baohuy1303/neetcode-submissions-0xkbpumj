f46411dad707122a2143120f54f2691661cd735e
Huy B. Huynh <huynhbaohuy130333@gmail.com>
2026-06-08 03:34:15 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        XOR = 0
+        for i in range(0, len(nums) + 1):
+            XOR = XOR ^ i
+        
+        for i in range(0, len(nums)):
+            XOR = XOR ^ nums[i]
+        
+        return XOR
